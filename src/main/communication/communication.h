8fd0613ea6431f62df9c4e4d6141de50dce52111
Paul Breugnot <paul.breugnot@univ-fcomte.fr>
2020-01-28 10:19:38 +0100

    resource_manager -> resource_container

--- src/main/communication/communication.h
+++ src/main/communication/communication.h
@@ -6,7 +6,7 @@
 #include <mpi.h>
 #include <unordered_map>
 
-#include "resource_manager.h"
+#include "resource_container.h"
 #include "readers_writers.h"
 
 namespace FPMAS {
--- src/test/communication/communication.cpp
+++ src/test/communication/communication.cpp
@@ -6,7 +6,7 @@ using namespace std::chrono_literals;
 #include "gtest/gtest.h"
 #include "test_utils/test_utils.h"
 #include "communication/communication.h"
-#include "communication/resource_manager.h"
+#include "communication/resource_container.h"
 
 using FPMAS::communication::ResourceContainer;
 using FPMAS::communication::MpiCommunicator;
@@ -84,10 +84,8 @@ class TestResourceHandler : public ResourceContainer {
 			MPI_Comm_rank(MPI_COMM_WORLD, &current_rank);
 			int size;
 			MPI_Comm_size(MPI_COMM_WORLD, &size);
-			for (int i = 0; i < size; i++) {
-				if(i != current_rank)
+			for (int i = 0; i < size; i++)
 					data[i] = i;
-			}
 		}
 
 		std::string getData(unsigned long id) const override {
@@ -131,10 +129,32 @@ TEST_F(Mpi_TerminationTest, termination_test_with_delay) {
 
 }
 
+/**
+ * The following test serie :
+ * - read_from_passive_procs_test
+ * - read_from_reading_procs_test
+ * - read_from_acquiring_procs_test
+ * - acquire_from_passive_procs_test
+ * - acquire_from_reading_procs_test
+ * - acquire_from_acquiring_procs_test
+ *
+ * ensures that procs are still able to handle request while they are
+ * themselves performing a request or waiting for the end of execution,
+ * preventing any deadlock.
+ *
+ * Notice that even if multiple request are performed simultaneously, no
+ * concurrency access is assumed is those examples (ie multiple procs trying to
+ * access the same ressource). This is tested in the readers_writers.cpp test serie.
+ */
+/**
+ * Tests if its possible to read a data from a passive proc (ie a proc waiting
+ * for termination after terminate() has been called.
+ */
 TEST_F(Mpi_TerminationTest, read_from_passive_procs_test) {
 	TerminableMpiCommunicator comm(handler);
 
 	if(comm.getSize() >= 2) {
+		// Each even proc asks for data to proc + 1 (if it exists)
 		if(comm.getRank() % 2 == 0 && comm.getRank() != comm.getSize() - 1) {
 			// Ensures that even procs enter passive mode
 			std::this_thread::sleep_for(100ms);
@@ -142,18 +162,59 @@ TEST_F(Mpi_TerminationTest, read_from_passive_procs_test) {
 			std::string data = comm.read(comm.getRank(), comm.getRank() + 1);
 			ASSERT_EQ(data, std::to_string(comm.getRank()));
 		}
+		// Odd procs immediatly enter passive mode
 		comm.terminate();
 	} else {
 		PRINT_MIN_PROCS_WARNING(read_from_passive_procs_test, 2);
 	}
 }
 
-TEST_F(Mpi_TerminationTest, read_from_active_procs_test) {
+/**
+ * Tests if its possible to read a data from an active proc performing an
+ * acquisition (ie a proc waiting for a response after acquie() has been called)
+ */
+TEST_F(Mpi_TerminationTest, read_from_acquiring_procs_test) {
 	TerminableMpiCommunicator comm(handler);
 
 	if(comm.getSize() >= 3) {
 		if(comm.getRank() == 1) {
-			// Delays the request reception
+			// Delays the acquire request reception from 0
+			std::this_thread::sleep_for(500ms);
+		}
+		else if (comm.getRank() == 0) {
+			// Will stay active for 500 ms due to proc 1 reception delay
+			// By this time, proc 2 should be able to get its response from
+			// this proc
+			std::string data = comm.acquire(0, 1);
+			ASSERT_EQ(data, "0");
+
+			comm.giveBack(0, 1);
+		}
+		else if (comm.getRank() == 2) {
+			// Ensures that proc 0 has initiated its request
+			std::this_thread::sleep_for(10ms);
+			// Asks data to the currently active proc
+			std::string data = comm.read(2, 0);
+			ASSERT_EQ(data, "2");
+		}
+		comm.terminate();
+	}
+	else {
+		PRINT_MIN_PROCS_WARNING(read_from_passive_procs_test, 3);
+	}
+
+}
+
+/**
+ * Tests if its possible to read a data from an active proc performing a
+ * reading (ie a proc waiting for a response after read() has been called)
+ */
+TEST_F(Mpi_TerminationTest, read_from_reading_procs_test) {
+	TerminableMpiCommunicator comm(handler);
+
+	if(comm.getSize() >= 3) {
+		if(comm.getRank() == 1) {
+			// Delays the read request reception from 0
 			std::this_thread::sleep_for(500ms);
 		}
 		else if (comm.getRank() == 0) {
@@ -178,10 +239,15 @@ TEST_F(Mpi_TerminationTest, read_from_active_procs_test) {
 
 }
 
+/**
+ * Tests if its possible to acquire a data from a passive proc (ie a proc waiting
+ * for termination after terminate() has been called.
+ */
 TEST_F(Mpi_TerminationTest, acquire_from_passive_procs_test) {
 	TerminableMpiCommunicator comm(handler);
 
 	if(comm.getSize() >= 2) {
+		// Each even proc asks for data to proc + 1 (if it exists)
 		if(comm.getRank() % 2 == 0 && comm.getRank() != comm.getSize() - 1) {
 			// Ensures that even procs enter passive mode
 			std::this_thread::sleep_for(100ms);
@@ -197,6 +263,7 @@ TEST_F(Mpi_TerminationTest, acquire_from_passive_procs_test) {
 			std::cout << "[" << comm.getRank() << "] give back" << std::endl;
 			comm.giveBack(comm.getRank(), comm.getRank() + 1);
 		}
+		// Odd procs immediatly enter passive mode
 		comm.terminate();
 		if(comm.getRank() % 2 == 1)
 			ASSERT_EQ(this->handler.data.at(comm.getRank() - 1), comm.getRank());
@@ -204,4 +271,87 @@ TEST_F(Mpi_TerminationTest, acquire_from_passive_procs_test) {
 	} else {
 		PRINT_MIN_PROCS_WARNING(read_from_passive_procs_test, 2);
 	}
+}
+
+/**
+ * Tests if it's possible to acquire a data from an active proc performing a
+ * reading (ie a proc waiting for a response after read() has been called)
+ */
+TEST_F(Mpi_TerminationTest, acquire_from_reading_procs_test) {
+	TerminableMpiCommunicator comm(handler);
+
+	if(comm.getSize() >= 3) {
+		if(comm.getRank() == 1) {
+			// Delays the read request reception from 0
+			std::this_thread::sleep_for(500ms);
+		}
+		else if (comm.getRank() == 0) {
+			// Will stay active, waiting for the reading request, for 500 ms
+			// due to proc 1 reception delay By this time, proc 2 should be
+			// able to get its response from this proc
+			std::string data = comm.read(0, 1);
+			ASSERT_EQ(data, "0");
+
+			ASSERT_EQ(this->handler.data[2], 10);
+		}
+		else if (comm.getRank() == 2) {
+			// Ensures that proc 0 has initiated its request
+			std::this_thread::sleep_for(10ms);
+			// Asks data to the currently active proc
+			std::string data = comm.acquire(2, 0);
+			ASSERT_EQ(data, "2");
+
+			this->handler.data[2] = std::stoul(data);
+			this->handler.data[2] = 10;
+
+			comm.giveBack(2, 0);
+		}
+		comm.terminate();
+	}
+	else {
+		PRINT_MIN_PROCS_WARNING(read_from_passive_procs_test, 3);
+	}
+
+}
+
+/**
+ * Tests if it's possible to acquire a data from an active proc performing an
+ * acquisition (ie a proc waiting for a response after acquire() has been called)
+ */
+TEST_F(Mpi_TerminationTest, acquire_from_acquiring_procs_test) {
+	TerminableMpiCommunicator comm(handler);
+
+	if(comm.getSize() >= 3) {
+		if(comm.getRank() == 1) {
+			// Delays the acquire request reception from 0
+			std::this_thread::sleep_for(500ms);
+		}
+		else if (comm.getRank() == 0) {
+			// Will stay active, waiting for the acquisition request, for 500 ms
+			// due to proc 1 reception delay. By this time, proc 2 should be
+			// able to get its response from this proc
+			std::string data = comm.acquire(0, 1);
+			ASSERT_EQ(data, "0");
+			comm.giveBack(0, 1);
+
+			ASSERT_EQ(this->handler.data[2], 10);
+		}
+		else if (comm.getRank() == 2) {
+			// Ensures that proc 0 has initiated its request
+			std::this_thread::sleep_for(10ms);
+			// Asks data to the currently active proc
+			std::string data = comm.acquire(2, 0);
+			ASSERT_EQ(data, "2");
+
+			this->handler.data[2] = std::stoul(data);
+			this->handler.data[2] = 10;
+
+			comm.giveBack(2, 0);
+		}
+		comm.terminate();
+	}
+	else {
+		PRINT_MIN_PROCS_WARNING(read_from_passive_procs_test, 3);
+	}
+
 }
