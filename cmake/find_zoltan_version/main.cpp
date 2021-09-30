af350c0e686854a4307c85f3ed971e6139576aaa
Paul Breugnot <paul.breugnot@univ-fcomte.fr>
2021-09-30 11:37:22 +0200

    :ambulance: Fixes find_version issues
    There were parsing issues with find_nlohmann_json_version and
    find_zoltan_version.
    
    Zoltan "3.90" was notably interpreted as "3.9", what broke the Zoltan
    "3.81" minimum dependency.

--- cmake/find_zoltan_version/main.cpp
+++ cmake/find_zoltan_version/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "zoltan.h"
 
+#define EXPAND_STR(VERSION_NUMBER) #VERSION_NUMBER
+#define STR(VERSION_NUMBER) EXPAND_STR(VERSION_NUMBER)
+
 int main() {
-	std::cout << ZOLTAN_VERSION_NUMBER;
+	std::cout << STR(ZOLTAN_VERSION_NUMBER);
 	return 0;
 }
