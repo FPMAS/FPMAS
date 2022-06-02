8659e3c051f81dd794a6c3dde97bd5dbed83a121
Paul Breugnot <paul.breugnot@univ-fcomte.fr>
2022-06-02 11:49:17 +0200

    :bookmark: v1.5.1

--- docs/search/files_6.js
+++ docs/search/files_6.js
@@ -3,7 +3,7 @@ var searchData=
   ['generator_2eh_0',['generator.h',['../api_2random_2generator_8h.html',1,'(Global Namespace)'],['../random_2generator_8h.html',1,'(Global Namespace)']]],
   ['ghost_5fmode_2eh_1',['ghost_mode.h',['../ghost__mode_8h.html',1,'']]],
   ['graph_2eh_2',['graph.h',['../api_2graph_2graph_8h.html',1,'(Global Namespace)'],['../graph_2graph_8h.html',1,'(Global Namespace)'],['../model_2spatial_2graph_8h.html',1,'(Global Namespace)']]],
-  ['graph_5fbuilder_2eh_3',['graph_builder.h',['../api_2graph_2graph__builder_8h.html',1,'(Global Namespace)'],['../graph_2graph__builder_8h.html',1,'(Global Namespace)']]],
+  ['graph_5fbuilder_2eh_3',['graph_builder.h',['../api_2graph_2graph__builder_8h.html',1,'(Global Namespace)'],['../graph_2graph__builder_8h.html',1,'(Global Namespace)'],['../model_2spatial_2graph__builder_8h.html',1,'(Global Namespace)']]],
   ['grid_2eh_4',['grid.h',['../api_2model_2spatial_2grid_8h.html',1,'(Global Namespace)'],['../model_2spatial_2grid_8h.html',1,'(Global Namespace)']]],
   ['grid_5fagent_5fmapping_2eh_5',['grid_agent_mapping.h',['../grid__agent__mapping_8h.html',1,'']]],
   ['grid_5fbuilder_2eh_6',['grid_builder.h',['../grid__builder_8h.html',1,'']]],
--- docs/search/typedefs_10.js
+++ docs/search/typedefs_10.js
@@ -7,5 +7,6 @@ var searchData=
   ['setagentlocalevent_4',['SetAgentLocalEvent',['../namespacefpmas_1_1api_1_1model.html#a04d5a3474ddc7cca0c6ec92a6fe7ad2d',1,'fpmas::api::model']]],
   ['setdistantnodecallback_5',['SetDistantNodeCallback',['../classfpmas_1_1api_1_1graph_1_1DistributedGraph.html#a612d62a4dc12cd6080250a21a57c95ee',1,'fpmas::api::graph::DistributedGraph']]],
   ['setlocalnodecallback_6',['SetLocalNodeCallback',['../classfpmas_1_1api_1_1graph_1_1DistributedGraph.html#aed786edc6522005381176cd77667fb98',1,'fpmas::api::graph::DistributedGraph']]],
-  ['subtimestep_7',['SubTimeStep',['../namespacefpmas_1_1api_1_1scheduler.html#a77af02d3aacc514067e2762f0d021261',1,'fpmas::api::scheduler']]]
+  ['smallworldgraphbuilder_7',['SmallWorldGraphBuilder',['../namespacefpmas_1_1model.html#a660af206510ce02d40b6dbd82b02b0a4',1,'fpmas::model']]],
+  ['subtimestep_8',['SubTimeStep',['../namespacefpmas_1_1api_1_1scheduler.html#a77af02d3aacc514067e2762f0d021261',1,'fpmas::api::scheduler']]]
 ];
