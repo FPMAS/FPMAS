b2ae28360b9e2fefc1882dcaa69c7dbc6b828ae8
Paul Breugnot <paul.breugnot@univ-fcomte.fr>
2022-10-13 15:10:07 +0200

    :art: SpatiaModel: CellType typedef

--- docs/search/typedefs_2.js
+++ docs/search/typedefs_2.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['cell_0',['Cell',['../classfpmas_1_1api_1_1model_1_1SpatialAgent.html#abf0a276b2503f729442c5b9811eb326d',1,'fpmas::api::model::SpatialAgent::Cell()'],['../namespacefpmas_1_1model.html#a3fe6779a379bc1fb37b53a569b54d29a',1,'fpmas::model::Cell()']]],
   ['cellmatrix_1',['CellMatrix',['../classfpmas_1_1model_1_1detail_1_1GridBuilder.html#ae57687750677bf10e7b08c49ed374761',1,'fpmas::model::detail::GridBuilder']]],
-  ['celltype_2',['CellType',['../structfpmas_1_1model_1_1GridConfig.html#a77d25613c703c274126081c6fa3e4b9f',1,'fpmas::model::GridConfig']]],
+  ['celltype_2',['CellType',['../classfpmas_1_1api_1_1model_1_1SpatialModel.html#a5026ed523e2b2a474780909566fbc1df',1,'fpmas::api::model::SpatialModel::CellType()'],['../structfpmas_1_1model_1_1GridConfig.html#a77d25613c703c274126081c6fa3e4b9f',1,'fpmas::model::GridConfig::CellType()']]],
   ['clock_3',['Clock',['../namespacefpmas_1_1api_1_1utils_1_1perf.html#a126ebf6b23629efe06bfee44d9ea4b77',1,'fpmas::api::utils::perf']]],
   ['clocktime_4',['ClockTime',['../namespacefpmas_1_1api_1_1utils_1_1perf.html#aafb99a0a2e2c516e7aa6c2dfa6871d78',1,'fpmas::api::utils::perf']]]
 ];
